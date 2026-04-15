af22958c671b873cb37e0a0c4d6eb7aedddce8df
YanMachadoCarrillo <yanca.next@gmail.com>
2026-04-14 23:25:50 -0500

    Update App.java

--- javaCal-main/mi-java/src/main/com/micro/java/App.java
+++ javaCal-main/mi-java/src/main/com/micro/java/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
+@CrossOrigin(origins = "*") // Permite peticiones desde otros dominios
 @SpringBootApplication
 @RestController
 public class App {
